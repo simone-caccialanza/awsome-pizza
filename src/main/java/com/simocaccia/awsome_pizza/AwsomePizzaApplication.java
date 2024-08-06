417f93b814cec58d0ec55bfa0ebd284bf75c8f28
simone-caccialanza <simone.caccialanza@gmail.com>
2024-08-06 20:57:47 +0200

    First commit

---
+++ src/main/java/com/simocaccia/awsome_pizza/AwsomePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.simocaccia.awsome_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsomePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsomePizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simocaccia/awsome_pizza/AwsomePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simocaccia.awsome_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsomePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
